622d26764c3317f19f62064520004dfb35420fb0
Atilla <atillafacebook@hotmail.fr>
2019-10-24 12:55:22 +0200

    preboost

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+100.times do
+  p = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  end
+
+100.times do
+ d = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, zip_code: Faker::Nation.capital_city, specialty: Faker::Science.element)
+ end
